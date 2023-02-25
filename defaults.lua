1b24ab35f427a0204693a2d45c07798a6ce25863
Dilip Chauhan <chauhandilip305@gmail.com>
2023-02-25 20:29:28 +0530

    Updated on Feb 25 08:29:28

--- defaults.lua
+++ defaults.lua
@@ -2,11 +2,10 @@
 -- DEFAULT APPLICATIONS
 default = {}
 
-
 default.terminal = "st"
 default.text_editor = "nvim"
 default.filemanager = "ranger"
-default.web_browser = "qutebrowser"
+default.web_browser = "firefox"
 default.image_viewer = "sxiv"
 default.systerm_monitor = "htop"
 default.pdf_viewer = "zathura"
